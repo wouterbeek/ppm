c67e1090aa0a755d4d4ceeb44eef58fe6a3d5143
Wouter Beek <wouter@triply.cc>
2017-08-04 10:52:30 +0200

    Various improvements.
    DOC: Document the assumptions made by WACK.
    
    ADDED: wack_ls/0 for enumerating all WACKs.
    
    ADDED: wack_updates/0 for enumerating all outdated WACKs.
    
    CHANGED: Removed wack/3 and wack_version/3 from public API.
    
    ENHANCED: wack_install/2 detects if a WACK is already installed.
    
    CLEAN: Reuse library(git) for Git invocation.
    
    ADDED: Ability to compare semantic versions.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # WACK
 
-A simple package manager for SWI-Prolog.
+A super-simple package manager for SWI-Prolog.
+
+To keep things super-simple, WACK makes the following assumptions:
+
+  1. Your package is hosted in a public Github repository.
+
+  2. Your package has a JSON file `wack.json' in its root that
+     contains the keys `owner' and `name'.
+
+  3. Your package uses Git annotated tags (`git tag -a vX.Y.Z -m '…'`)
+     that denote the semantic version (`vX.Y.Z`) of the package.
--- WACK.pl
+++ WACK.pl
@@ -1,26 +1,26 @@
 :- module(
-  wack,
+  'WACK',
   [
-    wack/3,         % ?Owner:atom, ?Repo:atom, ?Version:compound
     wack_install/2, % +Owner:atom, +Repo:atom
     wack_install/3, % +Owner:atom, +Repo:atom, +Version:compound
+    wack_ls/0,
     wack_remove/1,  % +Repo:atom
     wack_update/2,  % +Owner:atom, +Repo:atom
-    wack_version/3  % +Owner:atom, +Repo:atom, -Version:compound
+    wack_updates/0
   ]
 ).
 
 /** <module> WACK (Wouter pACK)
 
-A super simple alternative to SWI-Prolog Packs.
-
 @author Wouter Beek
-@version 2017/06
+@version 2017/06, 2017/08
 */
 
+:- use_module(library(ansi_term)).
 :- use_module(library(apply)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(filesex)).
+:- use_module(library(git)).
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
 :- use_module(library(lists)).
@@ -32,30 +32,10 @@ A super simple alternative to SWI-Prolog Packs.
 
   
 
-%! pack_dir(-PackDir:atom) is det.
-
-pack_dir(PackDir) :-
-  absolute_file_name(library(.), LibDir, [access(write),file_type(directory)]),
-  directory_file_path(LibDir, 'swipl/pack', PackDir),
-  (exists_directory(PackDir) -> true ; make_directory_path(PackDir)).
-
-
-
-%! version(?Version:compound)// is det.
-
-version(version(Major,Minor,Patch)) -->
-  "V",
-  integer(Major),
-  ".",
-  integer(Minor),
-  ".",
-  integer(Patch).
-
-
-
 %! wack(?Owner:atom, ?Repo:atom, ?Version:compound) is nondet.
 %
-% Enumerates currently installed WACKs and shows their version.
+% Enumerates currently installed WACKs together with their semantic
+% version.
 
 wack(Owner, Repo, Version) :-
   wack0(_, WackDict),
@@ -85,12 +65,34 @@ wack0(WackDir, WackDict) :-
 
 
 
+%! wack_ls is det.
+%
+% Display all currently installed WACKs.
+
+wack_ls :-
+  forall(
+    wack(Owner, Repo, Version),
+    wack_ls_row(Owner, Repo, Version)
+  ).
+
+wack_ls_row(Owner, Repo, Version) :-
+  phrase(version(Version), Codes),
+  format("~a\t~a\t~s", [Owner,Repo,Codes]).
+
+
+
 %! wack_install(+Owner:atom, +Repo:atom) is semidet.
 %! wack_install(+Owner:atom, +Repo:atom, +Version:compound) is semidet.
 %
 % Installs a WACK.  The latests version is chosen in case none is
 % specified.
 
+wack_install(Owner, Repo) :-
+  wack(Owner, Repo, CurrentVersion), !,
+  phrase(version(CurrentVersion), Codes),
+  format("Package ~a's ‘~a’ is already installed (version ~s)\n",
+         [Owner,Repo,Codes]),
+  format("Use wack_update/2 to update a package.\n").
 wack_install(Owner, Repo) :-
   wack_version_latest(Owner, Repo, LatestVersion),
   wack_install(Owner, Repo, LatestVersion).
@@ -103,11 +105,8 @@ wack_install(Owner, Repo, Version) :-
   uri_components(Uri, uri_components(https,'github.com',Path,_,_)),
   git([clone,Uri,'--branch',Tag,'--depth',1]),
   Version =.. [version|T],
-  format(
-    user_output,
-    "Successfully installed ~a's ‘~a’, version ~d.~d.~d\n",
-    [Owner,Repo|T]
-  ).
+  format("Successfully installed ~a's ‘~a’, version ~d.~d.~d\n",
+        [Owner,Repo|T]).
 
 
 
@@ -123,7 +122,7 @@ wack_remove(Repo) :-
 
 %! wack_update(+Owner:atom, +Repo:atom) is semidet.
 %
-% Updates an exisiting WACK
+% Updates an exisiting WACK.
 
 wack_update(Owner, Repo) :-
   wack(Owner, Repo, CurrentVersion),
@@ -134,11 +133,40 @@ wack_update(Owner, Repo) :-
       wack_install(Owner, Repo, LatestVersion),
       LatestVersion =.. [version|T],
       format(
-	user_output,
-	"Updated ~a's ‘~a’ to version ~d.~d.~d\n",
-	[Owner,Repo|T]
+        user_output,
+        "Updated ~a's ‘~a’ to version ~d.~d.~d\n",
+	      [Owner,Repo|T]
       )
   ).
+
+
+
+%! wack_updates is det.
+%
+% Shows packages, if any, that can be updated using wack_update/2.
+
+wack_updates :-
+  forall(
+    wack(Owner, Repo, CurrentVersion),
+    (
+      wack_version_latest(Owner, Repo, LatestVersion),
+      compare_version(Order, CurrentVersion, LatestVersion),
+      wack_updates_row(Owner, Repo, Order, CurrentVersion, LatestVersion)
+    )
+  ).
+
+wack_updates_row(_, _, =, _, _) :- !.
+wack_updates_row(Owner, Repo, Order, CurrentVersion, LatestVersion) :-
+  format("~a\t~a\t", [Owner,Repo]),
+  order_colors(Order, Color1, Color2),
+  phrase(version(CurrentVersion), CurrentCodes),
+  ansi_format([fg(Color1)], "~s", [CurrentCodes]),
+  format("\t»\t"),
+  phrase(version(LatestVersion), LatestCodes),
+  ansi_format([fg(Color2)], "~s", [LatestCodes]).
+
+order_colors(<, red, green).
+order_colors(>, green, red).
 
 
 
@@ -164,7 +192,7 @@ wack_version_latest(Owner, Repo, LatestVersion) :-
 
 
 
-% GITHUB %
+% SERVICE: GITHUB %
 
 %! github_open(+Segments:list(atom), -In:stream) is det.
 
@@ -195,28 +223,56 @@ github_version(Owner, Repo, Version) :-
 
 
 
-% GIT %
+% VC: GIT %
 
-%! git(+Args:list(atomic)) is det.
+%! git(+Arguments:list(atomic)) is det.
+%
+% Generic Git invocation that takes care of routing content from the
+% output and error streams.
 
-git(Args) :-
+git(Arguments) :-
   pack_dir(PackDir),
-  setup_call_cleanup(
-    process_create(
-      path(git),
-      Args,
-      [cwd(PackDir),process(Pid),stderr(pipe(Err)),stdout(pipe(Out))]
-    ),
-    (
-      thread_create(print_err(Err), ErrId, [at_exit(close(Err))]),
-      copy_stream_data(Out, user_output),
-      process_wait(Pid, exit(OutStatus)),
-      thread_signal(ErrId, thread_exit(ErrStatus)),
-      print_status(ErrStatus),
-      print_status(OutStatus)
-    ),
-    close(Out)
+  git(Arguments, [directory(PackDir)]).
+
+
+
+
+
+% VERSIONS %
+
+%! compare_version(?Order:oneof([<,=,>]), @Version1, @Version2) is det.
+%
+% Determine or test the order between two semantic versions.
+
+compare_version(Order, version(Major1,Minor1,Patch1),
+                version(Major2,Minor2,Patch2)) :-
+  compare(OrderMajor, Major1, Major2),
+  (   OrderMajor == =
+  ->  compare(OrderMinor, Minor1, Minor2),
+      (   OrderMinor == =
+      ->  compare(Order, Patch1, Patch2)
+      ;   Order = OrderMinor
+      )
+  ;   Order = OrderMajor
   ).
+
+
+
+%! version(?Version:compound)// is det.
+%
+% Parses/generates semantic versioning strings.
+%
+% @arg Version is a compound term of the form `version(int,int,int)'.
+
+version(version(Major,Minor,Patch)) -->
+  version_indicator,
+  integer(Major),
+  ".",
+  integer(Minor),
+  ".",
+  integer(Patch).
+version_indicator --> "v", !.
+version_indicator --> "V".
 
 
 
@@ -224,26 +280,50 @@ git(Args) :-
 
 % HELPERS %
 
-%! directory_path(+Dir, -Path) is nondet.
+%! directory_path(+Directory:atom, -File:atom) is nondet.
+%
+% Non-determinisitcally enumerates the Files that are in Directory.
+%
+% @arg Directory is an atom denoting a directory on the filesystem.
+%
+% @arg File is an atomic full path specifier of a file in Directory.
+%
+% The dummy files `.' and `..' are not included.
 
-directory_path(Dir, Path) :-
-  directory_files(Dir, Files),
+directory_path(Directory, Path) :-
+  directory_files(Directory, Files),
   member(File, Files),
   \+ is_dummy_file(File),
-  directory_file_path(Dir, File, Path).
+  directory_file_path(Directory, File, Path).
 
 
 
-%! is_dummy_file(+File) is semidet.
+%! is_dummy_file(+File:atom) is semidet.
+%
+% Succeeds if File is the local name of a dummy file, i.e., `.' or
+% `..'.
 
 is_dummy_file(.).
 is_dummy_file(..).
 
 
 
-%! print_err(+Err:stream) is det.
+%! pack_dir(-PackDir:atom) is det.
+%
+% @arg PackDir is bound to the directory used to store SWI packages
+%      in.
+%
+% Creates PackDir in case it does not yet exist.
 
-print_err(Err) :-
+pack_dir(PackDir) :-
+  absolute_file_name(pack(.), PackDir, [access(write),file_type(directory)]),
+  (exists_directory(PackDir) -> true ; make_directory_path(PackDir)).
+
+
+
+%! print_error(+Err:stream) is det.
+
+print_error(Err) :-
   repeat,
   read_stream_to_codes(Err, Codes, []),
   (   Codes == end_of_file
