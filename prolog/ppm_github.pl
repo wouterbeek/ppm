d976d5d848287bf6419645e05615b361b46fe950
Wouter Beek <wouter@triply.cc>
2018-07-07 14:31:52 +0200

    FIXED: Arity of github_version/3.

--- prolog/ppm_github.pl
+++ prolog/ppm_github.pl
@@ -97,10 +97,9 @@ github_uri(User, Repo, Uri) :-
 
 
 
-%! github_version(+User:atom, +Repo:atom, ?Version:compound,
-%!                -Id:atom) is nondet.
+%! github_version(+User:atom, +Repo:atom, ?Version:compound) is nondet.
 
-github_version(User, Repo, Version, Id) :-
+github_version(User, Repo, Version) :-
   github_open([repos,User,Repo,releases], [], 200, In),
   call_cleanup(
     json_read_dict(In, Dicts, [value_string_as(atom)]),
